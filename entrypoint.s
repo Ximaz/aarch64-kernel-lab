5df6cf7294c6b479f110f270e5bb59a4fa26e109
DURAND Malo <durand.malo.60590@gmail.com>
2026-01-21 05:38:32 -0500

    feat: adding memset function, bss initialized with zeroes

--- entrypoint.s
+++ entrypoint.s
@@ -1,7 +1,39 @@
 .section .text
 
+.global __bss_start
+.global __bss_end
+.global __stack_top
+
 _start:
-    LDR X0, =stack_top
+    # (__stack_top - _start) < 1MB, ADR works here
+    ADR X0, __stack_top
     MOV SP, X0
-    wfe
-    b _start
+
+    # Prepare the .bss section, filling memory with zeroes
+
+    # destination
+    # (__bss_start - _start) < 1MB, ADR works here
+    ADR X0, __bss_start
+    # byte
+    MOV X1, #0
+    # size computation
+    # (__bss_end - _start) < 1MB, ADR works here
+    ADR X2, __bss_end
+    SUB X2, X2, X0
+    BL _memset
+
+    WFE
+    B _start
+
+# X0 = destination
+# X1 = byte
+# X2 = size
+_memset:
+    CMP X2, #0
+    BEQ _memset_done
+    STRB W1, [X0], #1
+    SUB X2, X2, #1
+    B _memset
+
+_memset_done:
+    RET
