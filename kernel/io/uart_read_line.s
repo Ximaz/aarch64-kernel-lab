74d30558ead1a3511e252b0853ca173014e0f8c8
DURAND Malo <durand.malo.60590@gmail.com>
2026-01-22 16:37:35 -0500

    feat: handling backspace in uart_read_line

--- kernel/io/uart_read_line.s
+++ kernel/io/uart_read_line.s
@@ -10,6 +10,7 @@
 // Description
 //   This procedure reads a chunk of bytes from mini UART.
 //   If '\r' (0xd, null-terminator) is encountered, the procedure returns.
+//   If backspace (0x7f) is encountered, '\b' is printed, cursor gets seek.
 //   If the number of bytes read reaches 'size', the procedure returns.
 //   Each byte is stored sequentially in the 'dest' buffer.
 //   The procedure returns the total number of bytes read.
@@ -41,8 +42,20 @@ uart_read_line:
     AND W0, W0, #0xff
     CMP W0, #'\r'
     BEQ .done
+    CMP W0, #0x7f
+    BEQ .seek_cursor_left
     STRB W0, [X2], #1
     ADD X3, X3, #1
+    BL uart_write_byte
+    B .loop
+.seek_cursor_left:
+    SUB X3, X3, #1
+    SUB X2, X2, #1
+    MOV W0, #'\b'
+    BL uart_write_byte
+    MOV W0, #' '
+    BL uart_write_byte
+    MOV W0, #'\b'
     BL uart_write_byte
     B .loop
 .done:
